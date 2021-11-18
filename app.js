dd3c69f9b28d0eba406f15309d062ba5bd7b9e2b
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-18 17:08:49 +0300

    book list first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//Selecting Elements
+const UIbookForm = document.getElementById("book-form");
+const UItitle = document.getElementById("title");
+const UIauthor = document.getElementById("author");
+const UIisbn = document.getElementById("isbn");
+const UIbookList = document.getElementById("book-list");
+const UIcontainer = document.querySelector(".container");
+//Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+//UI Constructor
+function UI() {}
+UI.prototype.showAlert = function (message, className) {
+  //create a div
+  const div = document.createElement("div");
+  //add representative class
+  div.className = `alert ${className}`;
+  //add text
+  div.appendChild(document.createTextNode(message));
+  //get parent
+  const form = UIbookForm;
+  //insert alert before form
+  const container = UIcontainer;
+  container.insertBefore(div, form);
+  //disappear after 1.5 sec
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 1500);
+};
+UI.prototype.clearFields = function () {
+  UItitle.value = "";
+  UIauthor.value = "";
+  UIisbn.value = "";
+};
+UI.prototype.addBookToList = function (book) {
+  //create a tr element which will cal it "row"
+  const row = document.createElement("tr");
+  //insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a> </td>
+  `;
+  UIbookList.appendChild(row);
+};
+
+//Event Listeners
+UIbookForm.addEventListener("submit", function (event) {
+  //Form values
+  const title = UItitle.value;
+  const author = UIauthor.value;
+  const isbn = UIisbn.value;
+  //Creating new Book depending on form values
+  const book = new Book(title, author, isbn);
+  //Creating UI element
+  const ui = new UI();
+  //Validate
+  if (title === "" || author === "" || isbn === "") {
+    //Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    //Add book to list
+    ui.addBookToList(book); //ui objects addBook method, like a library.
+    //Clear field
+    ui.clearFields();
+  }
+
+  event.preventDefault();
+});
